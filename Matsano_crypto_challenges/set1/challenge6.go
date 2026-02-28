a707e2194be0c528474852aed5826a74c920aaa0
Emiliano1919 <emilianojg38@gmail.com>
2026-02-28 00:47:48 -0500

    Advance on challenge6

--- Matsano_crypto_challenges/set1/challenge6.go
+++ Matsano_crypto_challenges/set1/challenge6.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
 func BrianKernighan(v byte) int {
 	c := 0
 	for v > 0 {
@@ -24,10 +31,40 @@ func HammingDistance(a []byte, b []byte) int {
 	return count
 }
 
+type Result struct {
+	KSize int
+	Score float64
+}
+
 func main() {
 	test1 := `this is a test`
 	test2 := `wokka wokka!!!`
 	println(HammingDistance([]byte(test1), []byte(test2)))
+	data, err := os.ReadFile("Challenge6.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bigStr := strings.TrimSpace(string(data))
+	bytes := []byte(bigStr)
+	results := []Result{}
+	for i := 2; i < 40; i++ {
+		inter := HammingDistance(bytes[0:i], bytes[i:2*i])
+		final := float64(inter) / float64(i)
+		results = append(results, Result{i, final})
+	}
+
+	sort.Slice(results, func(a, b int) bool {
+		return results[a].Score < results[b].Score
+	})
+
+	best4 := results[0:4]
+	sum := 0.0
+	for _, r := range best4 {
+		sum += r.Score
+	}
+
+	avg := sum / float64(len(best4))
+	println(int(avg))
 
 	// bplain := ([]byte(plain))
 	// word := "ICE"
