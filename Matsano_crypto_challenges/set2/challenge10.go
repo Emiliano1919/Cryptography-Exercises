96882e20cbe453283b4921cfe4b9b4a928020b36
Emiliano1919 <emilianojg38@gmail.com>
2026-03-07 19:13:26 -0500

    Helper functions for CBC encryption with AES

--- Matsano_crypto_challenges/set2/challenge10.go
+++ Matsano_crypto_challenges/set2/challenge10.go
@@ -5,6 +5,8 @@ import (
 	"crypto/aes"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 )
 
 func decryptECB(key string, bytes []byte) string {
@@ -54,15 +56,80 @@ func padByteToNextMultipleOf(plaintext []byte, multipleOf int) []byte {
 	}
 	return result
 }
+func xorBytes(a, b []byte) []byte {
+	if len(a) != len(b) {
+		log.Fatal("Not same length")
+	}
+
+	result := make([]byte, len(a))
+	for i := range a {
+		result[i] = a[i] ^ b[i]
+	}
+	return result
+}
+
+// a is the in place
+func xorBytesInPlace(a, b []byte) {
+	if len(a) != len(b) {
+		panic("Nos same length")
+	}
+	for i := range a {
+		a[i] ^= b[i]
+	}
+}
+
+func encryptCBC(iv []byte, key string, bytes []byte) []byte {
+	keyBytes := []byte(key)
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	xorBytesInPlace(bytes[0:blockSize], iv)
+	for i := 0; i < len(bytes); i += blockSize {
+		if i != 0 {
+			// Here we do the XOR
+			xorBytesInPlace(bytes[i:i+blockSize], result[i-blockSize:i])
+		}
+		block.Encrypt(result[i:i+blockSize], bytes[i:i+blockSize])
+	}
+	return result
+}
+
+func decryptCBC(iv []byte, key string, bytes []byte) []byte {
+	keyBytes := []byte(key)
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	xorBytesInPlace(bytes[0:blockSize], iv)
+	for i := 0; i < len(bytes); i += blockSize {
+		if i != 0 {
+			// Here we do the XOR
+			xorBytesInPlace(bytes[i:i+blockSize], result[i-blockSize:i])
+		}
+		block.Decrypt(result[i:i+blockSize], bytes[i:i+blockSize])
+	}
+	return result
+}
 
 func main() {
 	key := "YELLOW SUBMARINE"
 	test := "QUE CLASE de perro es este? Un perritoooooo :)"
 	bytesTest := []byte(test)
 	paddedByteTest := padByteToNextMultipleOf(bytesTest, 16) // We need to pad it to an acceptable length multiple of 16
-	fmt.Printf("Result Byte Version: %q\n", paddedByteTest)
+	fmt.Printf("Result padding plaintext test: %q\n", paddedByteTest)
 	encryptedTest := encryptECB(key, paddedByteTest)
-	println(encryptedTest)
+	fmt.Printf("Result encryption test: %q\n", encryptedTest)
 	decryptedTest := decryptECB(key, []byte(encryptedTest))
-	fmt.Printf("Result Byte Version: %q\n", decryptedTest)
+	fmt.Printf("Result decryption test: %q\n", decryptedTest)
+	data, err := os.ReadFile("Challenge10.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+
 }
