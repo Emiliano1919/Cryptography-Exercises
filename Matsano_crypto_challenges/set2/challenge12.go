9e94d0a6074bc14a1765d9084bf68880a8edcb23
Emiliano1919 <emilianojg38@gmail.com>
2026-03-09 21:58:33 -0400

    Add helper functions to set up the Byte-at-a-time ECB decryption

---
+++ Matsano_crypto_challenges/set2/challenge12.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"log"
+)
+
+func decryptECB(key string, bytes []byte) string {
+	keyBytes := []byte(key)
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	for i := 0; i < len(bytes); i += blockSize {
+		block.Decrypt(result[i:i+blockSize], bytes[i:i+blockSize]) // We have to Decrypt it 16 bytes at a time
+	}
+	return string(result)
+}
+func encryptECB(key []byte, bytes []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	for i := 0; i < len(bytes); i += blockSize {
+		block.Encrypt(result[i:i+blockSize], bytes[i:i+blockSize]) // We have to encrypt it 16 bytes at a time
+	}
+	return result
+}
+
+func padByteVersion(plaintext []byte, size int) []byte {
+	padding := size - len([]byte(plaintext))
+
+	bpad := bytes.Repeat([]byte{byte(padding)}, padding)
+
+	return append(plaintext, bpad...)
+}
+
+func padByteToNextMultipleOf(plaintext []byte, multipleOf int) []byte {
+	currentSize := len(plaintext)
+	remainder := currentSize % multipleOf
+	var result []byte
+	if remainder == 0 {
+		result = padByteVersion(plaintext, currentSize)
+	} else {
+		nextSize := multipleOf - remainder + currentSize
+		result = padByteVersion(plaintext, nextSize)
+	}
+	return result
+}
+func xorBytes(a, b []byte) []byte {
+	if len(a) != len(b) {
+		log.Fatal("Not same length")
+	}
+
+	result := make([]byte, len(a))
+	for i := range a {
+		result[i] = a[i] ^ b[i]
+	}
+	return result
+}
+
+// a is the in place
+func xorBytesInPlace(a, b []byte) {
+	if len(a) != len(b) {
+		panic("Nos same length")
+	}
+	for i := range a {
+		a[i] ^= b[i]
+	}
+}
+
+func encryptCBC(iv []byte, key []byte, bytes []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	xorBytesInPlace(bytes[0:blockSize], iv)
+	for i := 0; i < len(bytes); i += blockSize {
+		if i != 0 {
+			// Here we do the XOR
+			xorBytesInPlace(bytes[i:i+blockSize], result[i-blockSize:i])
+		}
+		block.Encrypt(result[i:i+blockSize], bytes[i:i+blockSize])
+	}
+	return result
+}
+
+func decryptCBC(iv []byte, key string, bytes []byte) []byte {
+	keyBytes := []byte(key)
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]byte, len(bytes))
+	blockSize := block.BlockSize() // 16 bytes at a time
+	for i := 0; i < len(bytes); i += blockSize {
+		block.Decrypt(result[i:i+blockSize], bytes[i:i+blockSize])
+		if i == 0 {
+			xorBytesInPlace(result[i:i+blockSize], iv)
+		} else {
+			xorBytesInPlace(result[i:i+blockSize], bytes[i-blockSize:i])
+		}
+	}
+	return result
+}
+
+func randomKey16Bytes() []byte {
+	key := make([]byte, 16)
+	rand.Read(key)
+	return key
+}
+
+func encryption_oracle_ECB(plaintext []byte) []byte {
+	lastPlain := `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
+aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
+dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
+YnkK`
+	last, err := base64.StdEncoding.DecodeString(lastPlain)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastBytes := []byte(last)
+	plaintext = append(plaintext, lastBytes...)
+	plaintext = padByteToNextMultipleOf(plaintext, 16)
+	return []byte(encryptECB(stableKey, plaintext))
+}
+func counterOfRepeat(cipher []byte) int {
+
+	seen := make(map[string]struct{})
+	totalBlocks := 0
+
+	for i := 0; i+blockSize <= len(cipher); i += blockSize {
+		block := string(cipher[i : i+blockSize])
+		seen[block] = struct{}{}
+		totalBlocks++
+	}
+
+	return totalBlocks - len(seen)
+}
+
+func isECB(cipher []byte) {
+	if counterOfRepeat(cipher) > 0 {
+		println("\n It is ------ECB-------\n")
+	} else {
+		println("\n It is ------CBC-------\n")
+	}
+}
+
+const blockSize = 16 // The block size for AES
+var stableKey []byte
+
+func init() {
+	stableKey = make([]byte, 16)
+	rand.Read(stableKey)
+}
+
+func main() {
+	// This detection depends on Chosen Plaintext Attack (CPA)
+	var test string
+	for i := 0; i < 20; i++ {
+		test += "A"
+		bytesTest := []byte(test)
+		encryptedTest := encryption_oracle_ECB(bytesTest)
+		println(i)
+		println(len(bytesTest))
+		fmt.Printf("Result encryption test1: \n%q\n", encryptedTest)
+	}
+}
