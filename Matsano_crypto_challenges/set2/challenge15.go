2b22a2766ad5010e42ea3f64858d3458ba362499
Emiliano1919 <emilianojg38@gmail.com>
2026-03-15 21:35:24 -0400

    Add helper functions for padding validity under PKCS#7

---
+++ Matsano_crypto_challenges/set2/challenge15.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func padByteVersion(plaintext []byte, size int) []byte {
+	padding := size - len([]byte(plaintext))
+
+	bpad := bytes.Repeat([]byte{byte(padding)}, padding)
+
+	return append(plaintext, bpad...)
+}
+func padStringVersion(plaintext string, size int) string {
+	padding := size - len([]byte(plaintext))
+
+	bpad := bytes.Repeat([]byte{byte(padding)}, padding)
+	plaintext += string(bpad)
+
+	return plaintext
+}
+func padByteToNextMultipleOf(plaintext []byte, multipleOf int) []byte {
+	currentSize := len(plaintext)
+	remainder := currentSize % multipleOf
+	var result []byte
+	if remainder == 0 {
+		result = padByteVersion(plaintext, currentSize)
+	} else {
+		nextSize := multipleOf - remainder + currentSize
+		result = padByteVersion(plaintext, nextSize)
+	}
+	return result
+}
+
+func isValidPadding(paddedText []byte) bool{
+	
+}
+
+func main() {
+	test := "YELLOW SUBMARINE"
+
+	new1 := padByteVersion([]byte(test), 20)
+
+	fmt.Printf("Result Byte Version: %q\n", new1)
+	new2 := padStringVersion(test, 20)
+
+	fmt.Printf("Result String Version: %q\n", new2)
+}
