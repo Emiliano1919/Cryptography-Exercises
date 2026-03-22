0f1e95e4014b1c4b219b2698343cfe4e290b790d
Emiliano1919 <emilianojg38@gmail.com>
2026-03-21 23:29:06 -0400

    Working but inefficient and clunky padding validation

--- Matsano_crypto_challenges/set2/challenge15.go
+++ Matsano_crypto_challenges/set2/challenge15.go
@@ -18,14 +18,14 @@ const UnprintableASCII = "" +
 	"\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
 
 func padByteVersion(plaintext []byte, size int) []byte {
-	padding := size - len([]byte(plaintext))
+	padding := size - len(plaintext)
 
 	bpad := bytes.Repeat([]byte{byte(padding)}, padding)
 
 	return append(plaintext, bpad...)
 }
 func padStringVersion(plaintext string, size int) string {
-	padding := size - len([]byte(plaintext))
+	padding := size - len(plaintext)
 
 	bpad := bytes.Repeat([]byte{byte(padding)}, padding)
 	plaintext += string(bpad)
@@ -46,6 +46,7 @@ func padByteToNextMultipleOf(plaintext []byte, multipleOf int) []byte {
 }
 
 func isValidPaddingMultipleOf(paddedText []byte, multipleOf int) error {
+	fmt.Printf("\nResult input Version: %q\n", paddedText)
 	if multipleOf > 32 {
 		return MultiplicityError
 	}
@@ -55,9 +56,13 @@ func isValidPaddingMultipleOf(paddedText []byte, multipleOf int) error {
 	}
 	if bytes.ContainsAny(paddedText, UnprintableASCII) {
 		first := bytes.IndexAny(paddedText, UnprintableASCII)
-		withoutPadding := paddedText[0:first]
+		withoutPadding := make([]byte, first)
+		copy(withoutPadding, paddedText[:first])
+		fmt.Printf("Result without padding Version: %q\n", withoutPadding)
 		generatedPadding := padByteToNextMultipleOf(withoutPadding, multipleOf)
-		if string(generatedPadding) == string(paddedText) {
+		fmt.Printf("Result Generated Version: %q\n", generatedPadding)
+		fmt.Printf("Result Original Version: %q\n", paddedText)
+		if bytes.Equal(generatedPadding, paddedText) {
 			return nil
 		} else {
 			return PaddingError
@@ -70,6 +75,9 @@ func isValidPaddingMultipleOf(paddedText []byte, multipleOf int) error {
 
 func main() {
 	test := "YELLOW SUBMARINE"
+	test2 := []byte("ICE ICE BABY\x04\x04\x04\x04")
+	test3 := []byte("ICE ICE BABY\x05\x05\x05\x05")
+	test4 := []byte("ICE ICE BABY\x01\x02\x03\x04")
 
 	new1 := padByteVersion([]byte(test), 20)
 
@@ -78,6 +86,7 @@ func main() {
 
 	err := isValidPaddingMultipleOf([]byte(new1), 16)
 	if err != nil {
+		println("New1 kapput")
 		log.Println(err)
 	}
 	new2 := padByteToNextMultipleOf([]byte(test), 16)
@@ -86,6 +95,24 @@ func main() {
 	fmt.Printf("Result Byte Size: %d\n", len(new2))
 	err2 := isValidPaddingMultipleOf(new2, 16)
 	if err2 != nil {
+		println("New2 kapput")
 		log.Println(err2)
+	}
+
+	err3 := isValidPaddingMultipleOf(test2, 16)
+	if err3 != nil {
+		println("Test2 kapput")
+		log.Println(err3)
+	}
+	err4 := isValidPaddingMultipleOf(test3, 16)
+	if err4 != nil {
+
+		println("Test3 kapput")
+		log.Println(err4)
+	}
+	err5 := isValidPaddingMultipleOf(test4, 16)
+	if err5 != nil {
+		println("Test4 kapput")
+		log.Println(err5)
 	}
 }
