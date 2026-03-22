e9a06a18b83b35f822f95ae830ce92216fabc2a0
Emiliano1919 <emilianojg38@gmail.com>
2026-03-21 23:04:17 -0400

    Add initial version of validator with sentinel errors

--- Matsano_crypto_challenges/set2/challenge15.go
+++ Matsano_crypto_challenges/set2/challenge15.go
@@ -4,9 +4,18 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"log"
 )
 
-const PaddingError = errors.New("Incorrect Padding")
+var PaddingSizeError = errors.New("Incorrect Padding Size")
+var PaddingError = errors.New("Incorrect Padding")
+var MultiplicityError = errors.New("Multiplicity above 32")
+
+const UnprintableASCII = "" +
+	"\x00\x01\x02\x03\x04\x05\x06\x07" +
+	"\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
+	"\x10\x11\x12\x13\x14\x15\x16\x17" +
+	"\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
 
 func padByteVersion(plaintext []byte, size int) []byte {
 	padding := size - len([]byte(plaintext))
@@ -37,11 +46,26 @@ func padByteToNextMultipleOf(plaintext []byte, multipleOf int) []byte {
 }
 
 func isValidPaddingMultipleOf(paddedText []byte, multipleOf int) error {
+	if multipleOf > 32 {
+		return MultiplicityError
+	}
 	currentSize := len(paddedText)
 	if currentSize%multipleOf != 0 {
-		return PaddingError
-	} 
-	
+		return PaddingSizeError
+	}
+	if bytes.ContainsAny(paddedText, UnprintableASCII) {
+		first := bytes.IndexAny(paddedText, UnprintableASCII)
+		withoutPadding := paddedText[0:first]
+		generatedPadding := padByteToNextMultipleOf(withoutPadding, multipleOf)
+		if string(generatedPadding) == string(paddedText) {
+			return nil
+		} else {
+			return PaddingError
+		}
+
+	} else {
+		return nil
+	}
 }
 
 func main() {
@@ -50,7 +74,18 @@ func main() {
 	new1 := padByteVersion([]byte(test), 20)
 
 	fmt.Printf("Result Byte Version: %q\n", new1)
-	new2 := padStringVersion(test, 20)
+	fmt.Printf("Result Byte Size: %d\n", len(new1))
 
-	fmt.Printf("Result String Version: %q\n", new2)
+	err := isValidPaddingMultipleOf([]byte(new1), 16)
+	if err != nil {
+		log.Println(err)
+	}
+	new2 := padByteToNextMultipleOf([]byte(test), 16)
+
+	fmt.Printf("Result Byte Version: %q\n", new2)
+	fmt.Printf("Result Byte Size: %d\n", len(new2))
+	err2 := isValidPaddingMultipleOf(new2, 16)
+	if err2 != nil {
+		log.Println(err2)
+	}
 }
